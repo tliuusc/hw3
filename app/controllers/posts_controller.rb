b6df4ab80135280e91a97536a7cb64d8d6cb145f
Terry Liu <terry.liu@kellogg.northwestern.edu>
2023-05-18 02:25:34 +0000

    1

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+    @place = Place.find_by({ "id" => params["place_id"] })
+    @post["place_id"] = @place["id"]
+  end
+
+  def create
+    @post = Post.new
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post["phone_number"] = params["post"]["phone_number"]
+    @post["place_id"] = params["post"]["place_id"]
+    @post.save
+    redirect_to "/places/#{@post["place_id"]}"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources "posts"
+  resources "places"
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  get("/", { :controller => "posts", :action => "index" })
+  get("/", { :controller => "places", :action => "index" })
 end
